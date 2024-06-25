767473a1a14e66db4123308a84e07f23d9e353fb
Roney <theroneyv@gmail.com>
2024-06-25 11:43:59 -0400

    Update

---
+++ Clicks-Colors/achievements.js
@@ -0,0 +1,10 @@
+const ACHIEVEMENTS = {
+	"1": "You are the number one!",
+	"3": "It's the magic number",
+	"7": "Siuuu...!",
+	"13": "JaJaJa!",
+	"32": "Okey-dokey!",
+	"95": "Velocidad... Soy veloz!",
+	"101": "War... War never changes",
+	"666": "...",
+}
