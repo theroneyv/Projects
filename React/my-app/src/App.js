dd6a65c58edb0196b7f4195b556fbd974973fac6
Roney <theroneyv@gmail.com>
2024-06-26 20:55:11 -0400

    Update "React"

---
+++ React/my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Heart from './heart.png'
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={Heart} id="Heart"/>
+			</header>
+		</div>
+	);
+}
+
+export default App;
