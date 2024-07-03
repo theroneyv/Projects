cc5418bd6e356e1edca81e44fea57a52649d2ced
Roney <theroneyv@gmail.com>
2024-07-03 01:02:00 -0400

    Update "rock-paper-sissors"

--- Python/rock-paper-scissors.py
+++ Python/rock-paper-scissors.py
@@ -2,18 +2,21 @@ import random
 
 options = ['rock','paper','scissors']
 
-user = input('Your option: ')
-computer = random.choice(options)
+def main():
+    computer = random.choice(options)
+    while True:
+        user = input('Your option: ')
+        if user not in options:
+	        print('Invalid option!')
+        else:
+            if user == 'rock ' and computer == 'paper'or\
+                 user == 'paper' and computer == 'scissors' or\
+                 user == 'scissors' and computer == 'rock':
+        	    print('You lose!')
+            elif user == computer:
+	            print('Tie!')
+            else:
+	            print('You win!')
+            break
 
-if user not in options:
-	print('Invalid option!') 
-else:
-	print('Computer option:', computer)
-
-
-if user == 'rock ' and computer == 'paper' or user == 'paper' and computer == 'scissors' or user == 'scissors' and computer == 'rock':
-	print('You lose!')
-elif user == computer:
-	print('Tie!')
-else:
-	print('You win!')
+main()
